f890711388b3e893dc9488ad2a849050a70565e8
Emilianopp <emilianopp550@gmail.com>
2021-06-10 11:32:36 -0400

    add main.sh

--- Pipe/rnaSeq/code/cluster/cluster_job.sh
+++ Pipe/rnaSeq/code/cluster/cluster_job.sh
@@ -5,5 +5,5 @@
 
 module load r/4.0.0
 
-Rscript rnaSeq/code/cluster/${CLUSTERMETHOD}/${CLUSTERMETHOD}.R --data_output_directory rnaSeq/output/cluster/${CLUSTERMETHOD}/${DATASET} --console_output_directory rnaSeq/output/cluster/${CLUSTERMETHOD}/${DATASET} --input_directory rnaSeq/data/preprocessed/${DATASET} --name_dataset ${DATASET}
+Rscript ../../rnaSeq/code/cluster/${CLUSTERMETHOD}/${CLUSTERMETHOD}.R --data_output_directory ../../rnaSeq/output/cluster/${CLUSTERMETHOD}/${DATASET} --console_output_directory ../../rnaSeq/output/cluster/${CLUSTERMETHOD}/${DATASET} --input_directory ../../rnaSeq/data/preprocessed/${DATASET} --name_dataset ${DATASET}
 
