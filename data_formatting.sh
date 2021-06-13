75b0cc32fcbd49dc0adbf3e94afe974d0a454522
Emilianopp <emilianopp550@gmail.com>
2021-06-12 22:03:04 -0400

    syntax

--- data_formatting.sh
+++ data_formatting.sh
@@ -2,7 +2,7 @@
 #SBATCH --account=def-cdesouza
 #SBATCH --time=2:00:00
 #SBATCH --mem-per-cpu=200000M
-
+#sbatch --parsable --job-name=pre --output=/home/emiliano/projects/def-cdesouza/Lab/data/pythonLog.out /home/emiliano/projects/def-cdesouza/Lab/MRNA_pipe/data_formatting.sh
 conda activate env_full
 
-Python data_fromatting.py
+python data_fromatting.py
