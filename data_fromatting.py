8dbc5feaba6806f324fa0844db235328543589d3
Emilianopp <emilianopp550@gmail.com>
2021-06-14 11:59:35 -0400

    test

--- data_fromatting.py
+++ data_fromatting.py
@@ -1,8 +1,16 @@
 import subprocess
 import sys
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
+install("pandas")
+install("numpy")
 import pandas as pd
 import numpy as np
+
+
+
+
 p1 = "/home/emiliano/projects/def-cdesouza/Lab/data/GSE97930_FrontalCortex_snDrop-seq_UMI_Count_Matrix_08-01-2017.txt"
 
 p2 = "/home/emiliano/projects/def-cdesouza/Lab/data/GSE97930_CerebellarHem_snDrop-seq_UMI_Count_Matrix_08-01-2017.txt"
