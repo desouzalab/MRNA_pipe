ee370c8024bfd5c055b9f0f27091c753162c5267
Emilianopp <emilianopp550@gmail.com>
2021-06-14 12:23:07 -0400

    test

--- data_fromatting.py
+++ data_fromatting.py
@@ -1,10 +1,3 @@
-import subprocess
-import sys
-def install(package):
-    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
-
-install("pandas")
-install("numpy")
 import pandas as pd
 import numpy as np
 
