bfe878afcdb33b9db60728b8703d30ec0ea26aff
Emilianopp <emilianopp550@gmail.com>
2021-06-14 13:51:03 -0400

    bash script fixes

--- Directory_Constructor.sh
+++ Directory_Constructor.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 cd /home/emiliano/projects/def-cdesouza/Lab 
 mkdir data/raw/$DATA_NAME
-mkdir data/preprocessed/cluster/sc3/$DATA_NAME
-mkdir /MRNA_pipe/output/cluster/seurat/$DATA_NAME
-mkdir /MRNA_pipe/output/compare/AIR+VM+Purity/$DATA_NAME
-mkdir /MRNA_pipe/output/compare/chindex/$DATA_NAME
-mkdir /MRNA_pipe/output/compare/cMatrix/$DATA_NAME
-mkdir /MRNA_pipe/output/compare/heatmap/$DATA_NAME
-mkdir /MRNA_pipe/output/compare/regGenes/$DATA_NAME
-mkdir /MRNA_pipe/output/preprocess/$DATA_NAME
-mkdir /MRNA_pipe/output/visualize/tSNE/seurat/$DATA_NAME
-mkdir /MRNA_pipe/output/visualize/tSNE/sc3/$DATA_NAME
-mkdir /MRNA_pipe/output/visualize/tSNE+PCA/seurat/$DATA_NAME
-mkdir /MRNA_pipe/output/visualize/tSNE+PCA/sc3/$DATA_NAME
+mkdir data/preprocessed/$DATA_NAME
+mkdir MRNA_pipe/Pipe/rnaSeq/output/cluster/sc3/$DATA_NAME
+mkdir MRNA_pipe/Pipe/rnaSeq/output/cluster/seurat/$DATA_NAME
+mkdir MRNA_pipe/Pipe/rnaSeq/output/compare/AIR+VM+Purity/$DATA_NAME
+mkdir MRNA_pipe/Pipe/rnaSeq/output/compare/chIndex/$DATA_NAME
+mkdir MRNA_pipe/Pipe/rnaSeq/output/compare/cMatrix/$DATA_NAME
+mkdir MRNA_pipe/Pipe/rnaSeq/output/compare/heatmap/$DATA_NAME
+mkdir MRNA_pipe/Pipe/rnaSeq/output/compare/regGenes/$DATA_NAME
+mkdir MRNA_pipe/Pipe/rnaSeq/output/preprocess/$DATA_NAME
+mkdir MRNA_pipe/Pipe/rnaSeq/output/visualize/tSNE/seurat/$DATA_NAME
+mkdir MRNA_pipe/Pipe/rnaSeq/output/visualize/tSNE/sc3/$DATA_NAME
+mkdir MRNA_pipe/Pipe/rnaSeq/output/visualize/tSNE+PCA/seurat/$DATA_NAME
+mkdir MRNA_pipe/Pipe/rnaSeq/output/visualize/tSNE+PCA/sc3/$DATA_NAME
 
