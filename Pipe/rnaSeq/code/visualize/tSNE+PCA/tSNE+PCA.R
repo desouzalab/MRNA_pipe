ac6a2e82f5efea0717dbbc9a8d0e1aafa0c820e2
Emilianopp <emilianopp550@gmail.com>
2021-06-24 15:12:51 -0400

    debug

--- Pipe/rnaSeq/code/visualize/tSNE+PCA/tSNE+PCA.R
+++ Pipe/rnaSeq/code/visualize/tSNE+PCA/tSNE+PCA.R
@@ -89,11 +89,11 @@ if (length(all_preprocessed_ssRNASeq_files)==length(all_trueCluster_ssRNASeq_fil
     hommat=data.frame(tsneX,tsneY,TrueClusters,clusters)
 
     
-    tsnepca = ggplot(hommat, aes(y=tsneY,x=tsneX, color=as.factor(TrueClusters))) + geom_point(aes(shape=TrueClusters),size=1) + scale_shape_manual(values=c(0,1,2,3,4,5,6,8))
+    tsnepca = ggplot(hommat, aes(y=tsneY,x=tsneX, color=as.factor(clusters))) + geom_point(aes(shape=clusters),size=1) + scale_shape_manual(values=c(0,1,2,3,4,5,6,8))
     save_plot(paste0(outdir,"/TSNE+PCA_Colour_",c,"_",args$name_dataset,".pdf"),tsnepca)
     print("  ...plot tSNE+PCA colour")
 
-    tsnepca = ggplot(hommat, aes(y=tsneY,x=tsneX)) + geom_point(aes(shape=TrueClusters),size=1) + scale_shape_manual(values=c(0,1,2,3,4,5,6,8))
+    tsnepca = ggplot(hommat, aes(y=tsneY,x=tsneX)) + geom_point(aes(shape=clusters),size=1) + scale_shape_manual(values=c(0,1,2,3,4,5,6,8))
     save_plot(paste0(outdir,"/TSNE+PCA_Black_",c,"_",args$name_dataset,".pdf"),tsnepca)
     print("  ...plot tSNE+PCA black and white")
 
