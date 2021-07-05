d470910aec78a1ea69e144a25e7fe0fb4ab52b02
Emilianopp <emilianopp550@gmail.com>
2021-07-05 13:02:01 -0400

    test

--- Pipe/rnaSeq/code/cluster/seurat/seurat.R
+++ Pipe/rnaSeq/code/cluster/seurat/seurat.R
@@ -39,8 +39,9 @@ print(all_preprocessed_ssRNASeq_files)
 for (c in 1:length(all_preprocessed_ssRNASeq_files)){
   print(c)
   ### Create data frame
-  data=read.csv(file.path(args$input_directory, all_preprocessed_ssRNASeq_files[c]),row.names = 1 )
+  data=read.csv(file.path(args$input_directory, all_preprocessed_ssRNASeq_files[c]))
   print("  ...read")
+  print(head(data))
   print(head(data[,1:10]))
   ### Set row names for the data frame. Exclude the first column from the data frame.
 
@@ -76,55 +77,57 @@ for (c in 1:length(all_preprocessed_ssRNASeq_files)){
   pbmc <- NormalizeData(pbmc, normalization.method="LogNormalize", scale.factor=10000)
 
 
+  
+  
   # Identification of highly variable features (feature selection)
-  pbmc <- FindVariableFeatures(pbmc, selection.method="vst", nfeatures=2000)
+  pbmc <- FindVariableFeatures(data, selection.method="vst", nfeatures=2000)
   # Identify the 20 most highly variable genes
   top20 <- head(VariableFeatures(pbmc), 20)
-
+  
   outputString <- "20 MOST HIGHLY VARIABLE GENES:"
   print(outputString)
   print(top20)
-
+  
   # plot variable features with and without labels (give user an option to do this)
   plot1 <- VariableFeaturePlot(pbmc)
   plot2 <- LabelPoints(plot=plot1, points=top20, repel=TRUE)
   combpo=CombinePlots(plots=list(plot1, plot2))
-
+  
   # save plot (give user an option to do this)
   pdf(paste0(data_outdir,"/variableFeaturesWithoutLabels_",c,"_",args$name_dataset,".pdf"))
   plot1 
   dev.off()
   print("  ...plot variableFeaturesWithoutLabels")
-
+  
   pdf(paste0(data_outdir,"/variableFeaturesWithLabels_",c,"_",args$name_dataset,".pdf"))
   plot2
   dev.off()
   print("  ...plot variableFeaturesWithLabels")
-
-
+  
+  
   # Scaling the data
   all.genes <- rownames(pbmc)
   pbmc <- ScaleData(pbmc, features = all.genes)
-
+  
   # Perform linear dimensional reduction
   pbmc <- RunPCA(pbmc, features = VariableFeatures(object = pbmc))
-
+  
   # display PCA results (OPTIONAL)
   outputString <- "PCA RESULTS:"
   print(outputString)
   print(pbmc[["pca"]], dims = 1:5, nfeatures = 5)
-
-
-  # Determine the ‘dimensionality’ of the dataset
+  
+  
+  # Determine the ¡®dimensionality¡¯ of the dataset
   plot3=ElbowPlot(object=pbmc, ndims=50)
   save_plot(paste0(data_outdir,"/elbow_",c,"_",args$name_dataset,".pdf"),plot3)
   dev.off()
   print("  ...plot elbow")
-
-
+  
+  
   # Cluster the cells
-  pbmc <- FindNeighbors(pbmc, dims = 1:7)
-
+  pbmc <- FindNeighbors(pbmc, dims = 1:30)
+  
   # Not being used to find clusters
   # CH=matrix(c(paste("CH-",seq(0.06,0.19,by=0.01),sep=""),rep(0,14)),ncol=2)
   # kclust0=0
@@ -135,37 +138,37 @@ for (c in 1:length(all_preprocessed_ssRNASeq_files)){
   # clu=as.integer(clu[,1])
   # CH[i,2]=calinhara(t(data),clustering=clu,cn=max(clu))
   # }
-
+  
   # if(CH>CH0){
   # CH0<-CH
   # kclust=
   # res=i/100} 
   # Not being used up to this point
-
-
+  
+  
   # Clusters
   pbmc <- FindClusters(pbmc, resolution = 1)
-
+  print(pbmc)
   write.csv(Idents(pbmc), file=paste0(data_outdir,"/clustersSeurat_",c,"_",args$name_dataset,".csv"))
   print("  ...export to .csv")
-
+  
   # Plots
   plot5=DimPlot(pbmc, reduction = "pca")
   save_plot(paste0(data_outdir,"/PCA_",c,"_",args$name_dataset,".pdf"),plot5)
   print("  ...plot PCA")
-
-  pbmc <- RunUMAP(pbmc, dims = 1:10)
+  
+  pbmc <- RunUMAP(pbmc, dims = 1:30)
   plot6=DimPlot(pbmc, reduction = "umap")
   save_plot(paste0(data_outdir,"/UMAP_",c,"_",args$name_dataset,".pdf"),plot6)
   print("  ...plot UMAP")
-
+  
   
   pbmc=RunTSNE(pbmc)
   plot7=DimPlot(pbmc, reduction = "tsne")
   save_plot(paste0(data_outdir,"/TSNE_",c,"_",args$name_dataset,".pdf"),plot7)
   print("  ...plot TSNE")
   dev.off()
-
+  
   rm(data)
   rm(outputString)
   rm(pbmc)
