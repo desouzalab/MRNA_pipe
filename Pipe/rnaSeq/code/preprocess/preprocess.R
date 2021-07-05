25aeeb63675bc1e49da4b8d0a70c82ab570b7baf
Emilianopp <emilianopp550@gmail.com>
2021-07-05 13:13:11 -0400

    test

--- Pipe/rnaSeq/code/preprocess/preprocess.R
+++ Pipe/rnaSeq/code/preprocess/preprocess.R
@@ -1,7 +1,7 @@
 #======================================================================================================
-# PREPROCESSNG For data 58+16
+# PREPROCESSNG
 #======================================================================================================
-​
+
 #======================
 # libraries
 #======================
@@ -12,40 +12,40 @@ library(Seurat)
 library(patchwork)
 suppressMessages(library(cowplot))
 RSCRIPT <- "Rscript"
-​
-​
+
+
 #======================
 # arguments
 #======================
-​
+
 # create parser object
 parser <- ArgumentParser()
-​
+
 parser$add_argument("--name_dataset", type="character", help="name of the dataset")
 parser$add_argument("--input_directory", default="None", type="character", help="path to directory containing raw ssRNASeq data")
 parser$add_argument("--data_output_directory", type="character", help="Path to the preprocessed data output directory")
 parser$add_argument("--console_output_directory", type="character", help="Path to the .out file output directory")
-​
+
 args <- parser$parse_args()
 print(args)
-​
+
 data_outdir <- args$data_output_directory
 dir.create(file.path(data_outdir), showWarnings=FALSE)
-​
+
 console_outdir <- args$console_output_directory
 dir.create(file.path(console_outdir), showWarnings=FALSE)
-​
+
 all_raw_ssRNASeq_files <- list.files(args$input_directory, pattern="*.csv*")
 print(all_raw_ssRNASeq_files)
-​
+
 for (c in 1:length(all_raw_ssRNASeq_files)){
   cat(c)
   ### Create data frame
   data=read.csv(file.path(args$input_directory, all_raw_ssRNASeq_files[c]),row.names = 1)
   print("  ...read")
   print(head(data[,1:10]))
-  
-  seurat_object <- CreateSeuratObject(counts = data, project = "data_cutoff", min.cells = 3, min.features = 100)
+
+  seurat_object <- CreateSeuratObject(counts = data, project = "data3k", min.cells = 3, min.features = 200)
   seurat_object
   # focus on MT features: Low-quality / dying cells often exhibit extensive mitochondrial contamination
   seurat_object[["percent.mt"]] <- PercentageFeatureSet(seurat_object, pattern = "^MT-")
@@ -65,29 +65,30 @@ for (c in 1:length(all_raw_ssRNASeq_files)){
   print("  ...plot tSNE+PCA colour")
   dev.off()
   # based on figures, filtering (choose the threshold based on plots)
-  seurat_object <- subset(seurat_object, subset = nFeature_RNA>1700 & nFeature_RNA <3600 & nCount_RNA>49700)
+  seurat_object <- subset(seurat_object, subset = nFeature_RNA > 200 & nFeature_RNA < 2500 & percent.mt < 5)
   #Normalization
   pbmc <- NormalizeData(seurat_object, normalization.method = "LogNormalize", scale.factor = 10000)
   ### Set row names for the data frame. 
   #row.names(data)=data[,1]
-  
+
   ### Exclude the first column from the data frame.
   #data=data[,-1]
-  
+
   ### Exclude records where rowsum is less than 50
   #data=data[!rowSums(data)<50,]
-  
+
   ### Exclude records that have less than or equal to 864 zero's
   #data=data[!apply(data==0, 1, sum) <= 864, ]
   ### Export preprocessed data frame to CSV file
   outFilename <- paste0(data_outdir,"/preprocessed_",c,"_",args$name_dataset,".csv")
   write.csv(data, file=outFilename,row.names=TRUE)
   print("  ...export to .csv")
-  
+
   rm(data)
   rm(outFilename)
 }
-​
+
 print("DONE")
-​
+
 #======================================================================================================
+
