cc2dd5925cf21682c9186cf839f557d9647a94c9
Emilianopp <emilianopp550@gmail.com>
2021-07-08 13:53:01 -0400

    TESTS

--- Pipe/rnaSeq/code/cluster/cluster_job.sh
+++ Pipe/rnaSeq/code/cluster/cluster_job.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --account=def-cdesouza
 #SBATCH --time=02:00:00
-#SBATCH --cpus-per-task=2
-#SBATCH --mem-per-cpu=200000M
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=20000M
 
 module load r/4.0.0
 echo Rscript ../../rnaSeq/code/cluster/${CLUSTERMETHOD}/${CLUSTERMETHOD}.R --data_output_directory ../../rnaSeq/output/cluster/${CLUSTERMETHOD}/${DATASET} --console_output_directory ../../rnaSeq/output/cluster/${CLUSTERMETHOD}/${DATASET} --input_directory ~/projects/def-cdesouza/Lab/data/preprocessed/${DATASET} --name_dataset ${DATASET}
