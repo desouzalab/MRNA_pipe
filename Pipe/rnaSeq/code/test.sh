d8aadfe180161b95a96df158afb7f23674d7435c
emilianopp <emilianopp550@gmail.com>
2021-07-09 14:13:27 -0400

    redo giniclust plots

--- Pipe/rnaSeq/code/test.sh
+++ Pipe/rnaSeq/code/test.sh
@@ -1,7 +1,9 @@
-DATASET=GSE74672
-CLUSTERMETHOD=sc3
-module load r/4.0.0
+#!/bin/bash
 
-Rscript ../../rnaSeq/code/cluster/${CLUSTERMETHOD}/${CLUSTERMETHOD}.R --data_output_directory ../../rnaSeq/output/cluster/${CLUSTERMETHOD}/${DATASET} --console_output_directory ../../rnaSeq/output/cluster/${CLUSTERMETHOD}/${DATASET} --input_directory ~/projects/def-cdesouza/Lab/data/preprocessed/${DATASET} --name_dataset ${DATASET}
+module load r/4.0.0
+VISUALIZEMETHOD=tSNE+PCA
+DATASET=GSE98816+58
+CLUSTERMETHOD=sc3
+Rscript ../../rnaSeq/code/visualize/${VISUALIZEMETHOD}/${VISUALIZEMETHOD}.R --output_directory ../../rnaSeq/output/visualize/${VISUALIZEMETHOD}/${CLUSTERMETHOD}/${DATASET}/ --preprocessed_input_directory ~/projects/def-cdesouza/Lab/data/preprocessed/${DATASET}/ --true_cluster_input_directory ~/projects/def-cdesouza/Lab/data/true/${DATASET}/ --cluster_input_directory ../../rnaSeq/output/cluster/${CLUSTERMETHOD}/${DATASET}/ --name_dataset ${DATASET}
 
 
