d05e5ea29b039593812430f431c6c56c1643b21e
emilianopp <emilianopp550@gmail.com>
2021-07-09 13:55:20 -0400

    fix clusters

---
+++ Pipe/rnaSeq/code/test.sh
@@ -0,0 +1,7 @@
+DATASET=GSE74672
+CLUSTERMETHOD=sc3
+module load r/4.0.0
+
+Rscript ../../rnaSeq/code/cluster/${CLUSTERMETHOD}/${CLUSTERMETHOD}.R --data_output_directory ../../rnaSeq/output/cluster/${CLUSTERMETHOD}/${DATASET} --console_output_directory ../../rnaSeq/output/cluster/${CLUSTERMETHOD}/${DATASET} --input_directory ~/projects/def-cdesouza/Lab/data/preprocessed/${DATASET} --name_dataset ${DATASET}
+
+
